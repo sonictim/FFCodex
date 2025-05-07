5364d3af3cddf2c88686bdf420c5c6b2fd42f123
Tim Farrell <soundguru@gmail.com>
2025-05-06 20:26:43 -0500

    workinggg

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,28 @@
 pub use anyhow::{Result as R, anyhow};
-use claxon::input;
 use ffcodex_lib::*;
 
 fn main() -> R<()> {
     // Use the directly exported get_version function
     let version = chromaprint_bindings::get_version();
     println!("Chromaprint version: {}", version);
-    let input_file = "/Users/tfarrell/Desktop/subset test/THUNDER DUAL MONO 2.wav";
+    // let input_file = "/Users/tfarrell/Desktop/subset test/THUNDER DUAL MONO 2.wav";
 
-    let fp = get_fingerprint(input_file)?;
-    println!("Fingerprint: {}", fp);
-    // let output_file = "/Users/tfarrell/Desktop/subset test/THUNDER DUAL MONO 2.wav";
+    // let fp = get_fingerprint(input_file)?;
+    // println!("Fingerprint: {}", fp);
+    // // let output_file = "/Users/tfarrell/Desktop/subset test/THUNDER DUAL MONO 2.wav";
 
-    // flac_debug(input_file)?;
+    // // flac_debug(input_file)?;
 
-    let start_time = std::time::Instant::now();
+    // let start_time = std::time::Instant::now();
 
-    clean_multi_mono("/Users/tfarrell/Desktop/CRWDChld_PlaygroundVocals01_TF_TJFR copy.flac")?;
+    // clean_multi_mono("/Users/tfarrell/Desktop/CRWDChld_PlaygroundVocals01_TF_TJFR copy.flac")?;
 
-    // let mut c = Codex::new(input_file);
-    // c.convert_dual_mono()?;
-    // c.export(output_file)?;
+    // // let mut c = Codex::new(input_file);
+    // // c.convert_dual_mono()?;
+    // // c.export(output_file)?;
 
-    let elapsed_time = start_time.elapsed();
-    println!("Finished in {} seconds", elapsed_time.as_secs_f32());
+    // let elapsed_time = start_time.elapsed();
+    // println!("Finished in {} seconds", elapsed_time.as_secs_f32());
 
     // flac_debug(output_file)?;
 
