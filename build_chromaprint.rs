48777df93950a35d81a3c4c7013e465ae7b96748
sonictim <63481516+sonictim@users.noreply.github.com>
2025-05-06 20:50:10 -0500

    this should work for both now

--- build_chromaprint.rs
+++ build_chromaprint.rs
@@ -3,29 +3,47 @@ use std::path::PathBuf;
 
 pub fn build() {
     let root_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-    
+
     // Check if we're on Windows
     let target = env::var("TARGET").unwrap_or_default();
     let is_windows = target.contains("windows");
     let is_macos = target.contains("apple");
-    
+
     println!("cargo:warning=Using pre-compiled chromaprint static library");
-    
+
     // Set the search path for the chromaprint library
     if is_windows {
         // Use the Windows-specific library path
-        println!("cargo:rustc-link-search=native={}", root_path.join("resources").join("lib").join("x64").display());
+        println!(
+            "cargo:rustc-link-search=native={}",
+            root_path
+                .join("resources")
+                .join("lib")
+                .join("windows")
+                .join("x64")
+                .display()
+        );
     } else {
         // For other platforms
-        println!("cargo:rustc-link-search=native={}", root_path.join("resources").display());
+        println!(
+            "cargo:rustc-link-search=native={}",
+            root_path
+                .join("resources")
+                .join("lib")
+                .join("macos")
+                .display()
+        );
     }
-    
+
     // Link against the static library
     println!("cargo:rustc-link-lib=static=chromaprint");
-    
+
     // Add the include directory to the include path
-    println!("cargo:include={}", root_path.join("resources").join("include").display());
-    
+    println!(
+        "cargo:include={}",
+        root_path.join("resources").join("include").display()
+    );
+
     // Link against platform-specific C++ runtime libraries
     if is_macos {
         println!("cargo:rustc-link-lib=dylib=c++");
@@ -37,6 +55,6 @@ pub fn build() {
     } else {
         println!("cargo:rustc-link-lib=dylib=stdc++");
     }
-    
+
     println!("cargo:rerun-if-changed=build_chromaprint.rs");
 }
